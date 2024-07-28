2e7d60d7a6f2f20138697c973a9fd8c47b40ca55
Radu Rot <radu.rot@haufe.com>
2024-07-28 14:51:39 +0300

    fixes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,6 @@ import {LoginPageComponent} from "./components/login-page/login-page.component";
 import {RegisterPageComponent} from "./components/register-page/register-page.component";
 
 export const routes: Routes = [
-  {path:'login', component:LoginPageComponent},
+  {path:'', component:LoginPageComponent},
   {path:'register', component:RegisterPageComponent},
 ];
